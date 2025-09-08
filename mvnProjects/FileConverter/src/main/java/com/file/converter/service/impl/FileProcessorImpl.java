81102191cccf1d8849a1d8eef91f13e026adb734
oleksandr.stepurko <stepurko.alexandr@gmail.com>
2025-09-08 09:05:18 +0200

    CW19

--- mvnProjects/FileConverter/src/main/java/com/file/converter/service/impl/FileProcessorImpl.java
+++ mvnProjects/FileConverter/src/main/java/com/file/converter/service/impl/FileProcessorImpl.java
@@ -14,6 +14,7 @@ import java.nio.file.WatchEvent;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import lombok.SneakyThrows;
 
